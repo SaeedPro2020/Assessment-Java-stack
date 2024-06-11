d2341e06bf72845f17243b82e6e96cf1780ddac2
saeedfa20201@gmail.com <saeedfa20201@gmail.com>
2024-06-11 12:25:51 +0200

    Add data generator class

--- java-functional/src/main/java/com/example/Main.java
+++ java-functional/src/main/java/com/example/Main.java
@@ -1,21 +1,12 @@
 package com.example;
 
 import com.example.utility.*;
-import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        String uuid = UUIDGenerator.generateUUID();
-        String formattedDateTime = DateTimeGenerator.generateCurrentDateTime();
-        BigDecimal num1 = new BigDecimal("59.3297");
-        BigDecimal num2 = new BigDecimal("17.9868");
 
-        BigDecimal divisionResult = DivisionCalculator.calculateDivision(num1, num2);
-        BigDecimal roundedResult = ResultRounder.roundResult(divisionResult);
-        String calculationResult = CalculationResultGenerator.generateCalculationResult(num1, num2, roundedResult);
-        String md5Hash = MD5HashGenerator.generateMD5Hash(calculationResult);
-
-        Container container = new Container(uuid, formattedDateTime, num1, num2, roundedResult, calculationResult, md5Hash);
+            // Generate data using the utility method
+            Container container = DataGeneratorUtils.generateData();
         JsonFileWriter.writeToJsonFile(container, "output.json");
 
         System.out.println(container);
